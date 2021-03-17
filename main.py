5361d0ffc4bd1a88bb294380344023362f162a7b
young <751563005@qq.com>
2021-03-17 17:26:09 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,208 @@
+import pygame
+import sys
+import traceback
+import myplane
+import enemy
+
+from pygame.locals import *
+
+
+pygame.init()
+# 混音器初始化
+pygame.mixer.init()
+
+bg_size = width, height = 480, 700
+screen = pygame.display.set_mode(bg_size)
+pygame.display.set_caption("飞机大战")
+
+background = pygame.image.load("images/background.png").convert()
+
+# 载入游戏音乐
+pygame.mixer.music.load("sounds/game_music.ogg")
+pygame.mixer.music.set_volume(0.2)
+bullet_sound = pygame.mixer.Sound("sounds/bullet.wav")
+bullet_sound.set_volume(0.2)
+bomb_sound = pygame.mixer.Sound("sounds/use_bomb.wav")
+bomb_sound.set_volume(0.2)
+supply_sound = pygame.mixer.Sound("sounds/supply.wav")
+supply_sound.set_volume(0.2)
+get_bomb_sound = pygame.mixer.Sound("sounds/get_bomb.wav")
+get_bomb_sound.set_volume(0.2)
+get_bullet_sound = pygame.mixer.Sound("sounds/get_bullet.wav")
+get_bullet_sound.set_volume(0.2)
+upgrade_sound = pygame.mixer.Sound("sounds/upgrade.wav")
+upgrade_sound.set_volume(0.2)
+enemy3_fly_sound = pygame.mixer.Sound("sounds/enemy3_flying.wav")
+enemy3_fly_sound.set_volume(0.2)
+enemy1_down_sound = pygame.mixer.Sound("sounds/enemy1_down.wav")
+enemy1_down_sound.set_volume(0.2)
+enemy2_down_sound = pygame.mixer.Sound("sounds/enemy2_down.wav")
+enemy2_down_sound.set_volume(0.2)
+enemy3_down_sound = pygame.mixer.Sound("sounds/enemy3_down.wav")
+enemy3_down_sound.set_volume(0.5)
+me_down_sound = pygame.mixer.Sound("sounds/me_down.wav")
+me_down_sound.set_volume(0.2)
+
+
+def add_small_enemies(group1, group2, num):
+    for i in range(num):
+        e1 = enemy.SmallEnemy(bg_size)
+        group1.add(e1)
+        group2.add(e1)
+
+
+def add_mid_enemies(group1, group2, num):
+    for i in range(num):
+        e2 = enemy.MidEnemy(bg_size)
+        group1.add(e2)
+        group2.add(e2)
+
+
+def add_large_enemies(group1, group2, num):
+    for i in range(num):
+        e3 = enemy.LargeEnemy(bg_size)
+        group1.add(e3)
+        group2.add(e3)
+
+
+def main():
+    pygame.mixer.music.play(-1)
+
+    # 生成我方飞机
+    me = myplane.MyPlane(bg_size)
+    # 生成地方飞机
+    enemies = pygame.sprite.Group()
+
+    # 小型飞机
+    small_enemies = pygame.sprite.Group()
+    add_small_enemies(small_enemies, enemies, 15)
+    # 中型飞机
+    mid_enemies = pygame.sprite.Group()
+    add_mid_enemies(mid_enemies, enemies, 5)
+    # 大型飞机
+    large_enemies = pygame.sprite.Group()
+    add_large_enemies(large_enemies, enemies, 2)
+
+    # 中弹图片序列
+    e1_destroy_index = 0
+    e2_destroy_index = 0
+    e3_destroy_index = 0
+    me_destroy_index = 0
+
+
+    switch_image = True
+    delay = 100
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # 检测用户按键操作
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[K_w] or key_pressed[K_UP]:
+            me.move_up()
+        if key_pressed[K_s] or key_pressed[K_DOWN]:
+            me.move_down()
+        if key_pressed[K_a] or key_pressed[K_LEFT]:
+            me.move_left()
+        if key_pressed[K_d] or key_pressed[K_RIGHT]:
+            me.move_right()
+
+        screen.blit(background, (0, 0))
+
+        # 绘制大型机
+        for each in large_enemies:
+            if each.active:
+                each.move()
+                if switch_image:
+                    screen.blit(each.image1, each.rect)
+                else:
+                    screen.blit(each.image2, each.rect)
+                # 出现时候的音效
+                if each.rect.bottom == -50:
+                    enemy3_fly_sound.play(-1)
+            else:
+                # 毁灭
+                if not (delay % 3):
+                    if e3_destroy_index == 0:
+                        enemy3_down_sound.play()
+                    screen.blit(each.destroy_images[e3_destroy_index], each.rect)
+                    e3_destroy_index = (e3_destroy_index + 1) % 6
+                    if e3_destroy_index == 0:
+                        enemy3_fly_sound.stop()
+                        each.reset()
+
+        # 绘制中型敌机
+        for each in mid_enemies:
+            if each.active:
+                each.move()
+                screen.blit(each.image, each.rect)
+            else:
+                # 毁灭
+                if not (delay % 3):
+                    if e2_destroy_index == 0:
+                        enemy2_down_sound.play()
+                    screen.blit(each.destroy_images[e2_destroy_index], each.rect)
+                    e2_destroy_index = (e2_destroy_index + 1) % 4
+                    if e2_destroy_index == 0:
+                        each.reset()
+        # 绘制小型敌机
+        for each in small_enemies:
+            if each.active:
+                each.move()
+                screen.blit(each.image, each.rect)
+            else:
+                # 毁灭
+                if not (delay % 3):
+                    if e1_destroy_index == 0:
+                        enemy1_down_sound.play()
+                    screen.blit(each.destroy_images[e1_destroy_index], each.rect)
+                    e1_destroy_index = (e1_destroy_index + 1) % 4
+                    if e1_destroy_index == 0:
+                        each.reset()
+
+        # 碰撞检测 ( pygame.sprite.collide_mask 表示将图片的透明部分删除后做碰撞检测 )
+        enemies_down = pygame.sprite.spritecollide(me, enemies, False, pygame.sprite.collide_mask)
+        if enemies_down:
+            # me.active = False
+            for e in enemies_down:
+                e.active = False
+
+        # 绘制我方战机
+        if me.active:
+            if switch_image:
+                screen.blit(me.image1, me.rect)
+            else:
+                screen.blit(me.image2, me.rect)
+        else:
+            # 毁灭
+            me_down_sound.play()
+            if not (delay % 3):
+                screen.blit(me.destroy_images[me_destroy_index], me.rect)
+                me_destroy_index = (me_destroy_index + 1) % 4
+                if e2_destroy_index == 0:
+                    print("Game Over")
+                    pygame.quit()
+
+        if not (delay % 5):
+            switch_image = not switch_image
+        delay -= 1
+        if not delay:
+            delay = 100
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
+    except Exception:
+        traceback.print_exception()
+        pygame.quit()
+        input()
